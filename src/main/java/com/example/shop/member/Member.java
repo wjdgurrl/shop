0bf7d16bc9ca9f0751d04cfb91dabd03ac13fd8d
gurrl <gurrl8356@gmail.com>
2025-01-10 15:28:51 +0900

    // jwt방식 위해서 복사본 생성

--- src/main/java/com/example/shop/member/Member.java
+++ src/main/java/com/example/shop/member/Member.java
@@ -1,12 +1,18 @@
 package com.example.shop.member;
 
+import com.example.shop.sales.Sales;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@ToString
 public class Member {
 
     @Id
@@ -17,4 +23,9 @@ public class Member {
     private String username;
     private String displayName;
     private String password;
+
+    @ToString.Exclude //무한 상호참조 방지
+    @OneToMany(mappedBy = "member")// 이 컬럼을 쓰는 다른 컬럼명 넣어주기()
+    private List<Sales> salse = new ArrayList<>(); //타입은 다른 테이블
+    //누군가 멤버 테이블을 행을 출력 시 멤버가 기록된 sales 행도 전부 출력함
 }
--- src/main/java/com/example/shop/sales/Sales.java
+++ src/main/java/com/example/shop/sales/Sales.java
@@ -23,7 +23,9 @@ public class Sales {
     private Integer price;
 
     //현재는 멤버 객체를 변수로 안받아서 아래 기능을 못씀
-   /* @ManyToOne
+   /* @ManyToOne(fetch = FetchType.Lazy)
+   // FetchType.EAGER를 넣으면 "이거 필요없어도 다른 테이블 항상 가져와주세요" 라는 뜻이고
+   // FetchType.LAZY를 넣으면 "게으르게 필요할 때만 가져와주세요" 라는 뜻입니다.
     @JoinColumn
             (name = "member_id",
                     foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)//Foreign key 제약사항을 자동으로 걸지 말라는 뜻
